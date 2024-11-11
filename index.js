2247a0755a6b016e5a1114ef42d50e0fd6a08800
Chandan Singh <pawarchandan@gmail.com>
2024-11-11 20:50:41 +0530

    update postgre database

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express"
 import bodyParser from "body-parser"
 import pg from "pg"
+import dotenv from 'dotenv';
+dotenv.config()
 
 const db = new pg.Client({
-    user:"postgres",
-    password:"pawar@abc",
-    host:"localhost",
-    port:5432,
-    database:"purchasing item"
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
 db.connect()
+  .then(() => console.log("Connected to PostgreSQL database"))
+  .catch(err => console.error("Connection error", err.stack));
 
 const app=express()
 const port=3000;
